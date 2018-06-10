36ace4f43c499c91628bf9e182d4ab37b179c716
Aleksandr Jashhuk <richmanfx@yandex.ru>
2018-06-10 20:54:41 +0300

    Логирование через syslog. Чтение всех параметров из конфигурационного файла. Добавлена минимальная скорость кулеров в конфиг файл.

--- c_amd_fan.c
+++ c_amd_fan.c
@@ -6,70 +6,86 @@
 #include <stdlib.h>
 #include <string.h>
 #include <libconfig.h>
+#include <syslog.h>
 
 
 void make_full_config_name(char *config_dir_name, char *config_file_name, char *full_config_name);
-void string_value_config_read(char *config_file_name, char *parameter_name, char *);
+void string_value_config_read(char *config_file_name, char *parameter_name, const char *local_value);
 int int_value_config_read(char *config_file_name, char *parameter_name);
+int get_gpu_number();
 
 void main()
 {
+
+    openlog("c_amd_fan", LOG_PERROR, LOG_USER);
+    syslog(LOG_DEBUG, "Start 'c_amd_fan'");
+
     char *config_file_name = "amd_fan.cfg";
     char *config_dir_name = "/usr/local/etc/amd_fan";
 
-    char debug_level[] = "";
-    char debug_level_parameter_name[] = "DEBUG_LEVEL";
-
+    char debug_level[] = "DEBUG";
     int high_temp;
-    char high_temp_parameter_name[] = "HIGH_TEMP";
-
     int low_temp;
-    char low_temp_parameter_name[] = "LOW_TEMP";
+    int sleep_time;
+    int speed_step;
+    int init_fan_speed;
+    int min_fan_speed;
+    int gpu_number;
 
 
-    // Full name of the configuration file
+    /***************************************/
+    /* Full name of the configuration file */
+    /***************************************/
     char full_config_file_name[50] = "";
     make_full_config_name(config_dir_name, config_file_name, full_config_file_name);
 
-    // Read parameters from config file
 
-    string_value_config_read(full_config_file_name, debug_level_parameter_name, debug_level);
-    printf("Уровень отладки: %s\n\n", debug_level);
+    /***************************************/
+    /*  Read parameters from config file   */
+    /***************************************/
+    string_value_config_read(full_config_file_name, "DEBUG_LEVEL", debug_level);
+    high_temp = int_value_config_read(full_config_file_name, "HIGH_TEMP");
+    low_temp = int_value_config_read(full_config_file_name, "LOW_TEMP");
+    sleep_time = int_value_config_read(full_config_file_name, "SLEEP_TIME");
+    speed_step = int_value_config_read(full_config_file_name, "SPEED_STEP");
+    init_fan_speed = int_value_config_read(full_config_file_name, "INIT_FAN_SPEED");
+    min_fan_speed = int_value_config_read(full_config_file_name, "MIN_FAN_SPEED");
 
-    high_temp = int_value_config_read(full_config_file_name, high_temp_parameter_name);
-    printf("Верхний уровень температуры: %d\n\n", high_temp);
 
-    low_temp = int_value_config_read(full_config_file_name, low_temp_parameter_name);
-    printf("Нижний уровень температуры: %d\n", low_temp);
+    /************************************/
+    /*          Get GPU number          */
+    /************************************/
+    gpu_number = get_gpu_number();
 
 
-
+    closelog();
 }
 
-void string_value_config_read(char *config_file_name, char *parameter_name, char *value)
-{
+int get_gpu_number() {
+
+    int gpu_number = 0;
+
+    syslog(LOG_DEBUG,"GPU count: %d\n\n", gpu_number);
+    return gpu_number;
+}
+
+void string_value_config_read(char *config_file_name, char *parameter_name, const char *local_value) {
     config_t cfg;
-    const char *parameter_value;
 
     // Инициализация
     config_init(&cfg);
 
     // Читать файл. Если ошибка, то завершить работу
-    if(! config_read_file(&cfg, config_file_name))
-    {
-        fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg), config_error_line(&cfg), config_error_text(&cfg));
-
-        // Освободить память обязательно, если это не конец программы
-        config_destroy(&cfg);
+    if(! config_read_file(&cfg, config_file_name)) {
+        syslog(LOG_ERR, "%s:%d - %s\n", config_error_file(&cfg), config_error_line(&cfg), config_error_text(&cfg));
+        config_destroy(&cfg);   // Освободить память обязательно, если это не конец программы
     }
 
     // Поиск значения
-    if(config_lookup_string(&cfg, parameter_name, &parameter_value))
-        printf("%s: %s\n", parameter_name, parameter_value);
+    if(config_lookup_string(&cfg, parameter_name, &local_value))
+        syslog(LOG_DEBUG, "%s: %s\n", parameter_name, local_value);
     else
-        fprintf(stderr, "No '%s' setting in configuration file.\n", parameter_name);
-
-    strcat(value, parameter_value);
+        syslog(LOG_ERR, "Parameter '%s' not found in config file", parameter_name);
 
     // Освободить память обязательно, если это не конец программы
     config_destroy(&cfg);
@@ -77,8 +93,7 @@ void string_value_config_read(char *config_file_name, char *parameter_name, char
 }
 
 
-int int_value_config_read(char *config_file_name, char *parameter_name)
-{
+int int_value_config_read(char *config_file_name, char *parameter_name) {
     config_t cfg;
     int parameter_value;
 //    config_setting_t *setting;
@@ -93,7 +108,7 @@ int int_value_config_read(char *config_file_name, char *parameter_name)
     // Читать файл. Если ошибка, то завершить работу
     if(! config_read_file(&cfg, config_file_name))
     {
-        fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg), config_error_line(&cfg), config_error_text(&cfg));
+        syslog(LOG_ERR, "%s:%d - %s\n", config_error_file(&cfg), config_error_line(&cfg), config_error_text(&cfg));
 
         // Освободить память обязательно, если это не конец программы
         config_destroy(&cfg);
@@ -101,16 +116,10 @@ int int_value_config_read(char *config_file_name, char *parameter_name)
     }
 
     // Поиск значения
-
-//    if(config_lookup_string(&cfg, "DEBUG_LEVEL", &str))
-//        printf("DEBUG_LEVEL: %s\n", str);
-//    else
-//        fprintf(stderr, "No 'DEBUG_LEVEL' setting in configuration file.\n");
-
     if(config_lookup_int(&cfg, parameter_name, &parameter_value))
-        printf("%s: %d\n", parameter_name, parameter_value);
+        syslog(LOG_DEBUG, "%s: %d\n", parameter_name, parameter_value);
     else
-        fprintf(stderr, "No '%s' setting in configuration file.\n", parameter_name);
+        syslog(LOG_ERR, "Parameter '%s' not found in config file", parameter_name);
 
     // Освободить память обязательно, если это не конец программы
     config_destroy(&cfg);
@@ -118,14 +127,11 @@ int int_value_config_read(char *config_file_name, char *parameter_name)
 
 }
 
-void make_full_config_name(char *config_dir_name, char *config_file_name, char *full_config_name)
-{
+void make_full_config_name(char *config_dir_name, char *config_file_name, char *full_config_name) {
     // Полное имя файла конфигурации
     strcat(full_config_name, config_dir_name);
     strcat(full_config_name, "/");
     strcat(full_config_name, config_file_name);
 
-    printf("Read configuration from file '%s'\n", full_config_name);
-
-    // return full_config_name;
+    syslog(LOG_DEBUG, "Read configuration from file '%s'", full_config_name);
 }
